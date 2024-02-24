d46af59931a5c8834f31f82b45686c2134a2eeb3
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2024-02-25 00:41:32 +0500

    change producer lifetime

--- ImageHosting.Storage/Features/Images/Extensions/ServiceCollectionExtensions.cs
+++ ImageHosting.Storage/Features/Images/Extensions/ServiceCollectionExtensions.cs
@@ -1,3 +1,4 @@
+using ImageHosting.Storage.Features.Images.Handlers;
 using ImageHosting.Storage.Features.Images.Services;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,12 +8,13 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddImageServices(this IServiceCollection services)
     {
+        services.AddSingleton<INewImageProducer, NewImageProducer>();
+        
         return services
             .AddTransient<IFileService, FileService>()
             .AddTransient<IFileUploadCommandFactory, FileUploadCommandFactory>()
             .AddTransient<IMetadataService, MetadataService>()
             .AddTransient<IMetadataUploadCommandFactory, MetadataUploadCommandFactory>()
-            .AddTransient<INewImageProducer, NewImageProducer>()
             .AddTransient<IPublishNewMessageCommandFactory, PublishNewMessageCommandFactory>()
             .AddTransient<IUploadFileService, UploadFileService>();
     }
