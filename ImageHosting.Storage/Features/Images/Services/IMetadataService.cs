202e620a0264a6b8183a35ed8fce6300f9c66380
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2024-02-25 00:43:19 +0500

    remove UploadImageRequest rename ReadImageResponse

--- ImageHosting.Storage/Features/Images/Services/IMetadataService.cs
+++ ImageHosting.Storage/Features/Images/Services/IMetadataService.cs
@@ -13,7 +13,7 @@ public interface IMetadataService
     /// <param name="imageMetadata">Metadata to write.</param>
     /// <param name="cancellationToken">A <see cref="System.Threading.CancellationToken" /> to observe while waiting for the task to complete.</param>
     /// <returns>Created entity.</returns>
-    Task<ReadImageResponse> WriteMetadataAsync(ImageMetadata imageMetadata, CancellationToken cancellationToken = default);
+    Task<ImageUploadedResponse> WriteMetadataAsync(ImageMetadata imageMetadata, CancellationToken cancellationToken = default);
 
     /// <summary>
     /// Delete metadata.
