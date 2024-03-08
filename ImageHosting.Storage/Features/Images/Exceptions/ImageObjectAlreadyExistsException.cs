24ce6db18fcedc1e87ea1d45c92f18ced319fae9
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2024-03-08 13:46:24 +0500

    rename ImageObjectAlreadyExistsException

--- ImageHosting.Storage/Features/Images/Exceptions/ImageObjectAlreadyExistsException.cs
+++ ImageHosting.Storage/Features/Images/Exceptions/ImageObjectAlreadyExistsException.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace ImageHosting.Storage.Features.Images.Exceptions;
 
-public class ImageObjectAlreadyExists(string bucketName, string imageId)
+public class ImageObjectAlreadyExistsException(string bucketName, string imageId)
     : Exception($"Image id {imageId} already exists in bucket {bucketName}.")
 {
     public string BucketName { get; } = bucketName;
