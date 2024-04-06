9086d01fe3d87fe9c551fbc6ae3a6a0ba0663ef1
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2024-04-06 23:05:32 +0500

    remove bucket id from CategorizedNewImage model

--- ImageHosting.Storage/Features/Images/Models/CategorizedNewImage.cs
+++ ImageHosting.Storage/Features/Images/Models/CategorizedNewImage.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Confluent.Kafka;
+using ImageHosting.Persistence.ValueTypes;
 
 namespace ImageHosting.Storage.Features.Images.Models;
 
-public class CategorizedNewImage : NewImage
+public class CategorizedNewImage
 {
+    [JsonPropertyName("imageId")] public required ImageId ImageId { get; init; }
     [JsonPropertyName("categories")] public required Dictionary<string, double> Categories { get; init; }
+
+    public void Deconstruct(out ImageId imageId, out Dictionary<string, double> categories)
+    {
+        imageId = ImageId;
+        categories = Categories;
+    }
 }
 
 public class CategorizedNewImageDeserializer : IDeserializer<CategorizedNewImage>
