61f1896dc02f1b93093c932b47587be9ddacd22a
Ivan Kozelskikh <ivankozelskikh@gmail.com>
2024-04-20 23:32:27 +0500

    remove buckerId property

--- ImageHosting.Storage/Features/Images/Models/CategorizedNewImage.cs
+++ ImageHosting.Storage/Features/Images/Models/CategorizedNewImage.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 using ImageHosting.Persistence.ValueTypes;
@@ -7,7 +6,6 @@ namespace ImageHosting.Storage.Features.Images.Models;
 
 public class CategorizedNewImage
 {
-    [JsonPropertyName("bucketId")] public Guid BucketId { get; init; }
     [JsonPropertyName("imageId")] public required ImageId ImageId { get; init; }
     [JsonPropertyName("categories")] public required Dictionary<string, double> Categories { get; init; }
 }
